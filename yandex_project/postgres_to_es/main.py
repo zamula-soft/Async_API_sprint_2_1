390de818a660dd2a1c1a2475849fd27f2af85d1e
кирилл бондарь <ironbondar@gmail.com>
2022-07-25 09:45:39 +0300

    fix etl process

--- yandex_project/postgres_to_es/main.py
+++ yandex_project/postgres_to_es/main.py
@@ -1,9 +1,12 @@
-from time import sleep
 from datetime import datetime
+from time import sleep
 
-from postgres_d import PGLoader
 from elastics import ESLoader
+from postgres_d import PGLoader
 from redis_work import Status
+from utils import get_logger
+
+logger = get_logger(__name__)
 
 
 def transfer_data():
@@ -17,17 +20,46 @@ def transfer_data():
 
     mod_date = status.get_status('mod_date')
     mod_date = mod_date.decode('utf-8') if mod_date else '2020-01-01 00:00:00'
-    new_date = f'{datetime.now()}'
+    new_date = f'{datetime.utcnow()()}'# тут надо часовой пояс указывать, иначе рассинхрон пойдет
+    logger.debug('film_work datetime before update : %s', mod_date)
+    logger.debug('film_work datetime after update : %s', new_date)
 
     for movies in pg_loader.get_movies_from_database(mod_date):
         es_loader.save_movies(movies)
 
     status.set_status('mod_date', new_date)
+
+    # жанры сделал в твоем стиле
+    last_genre_date = status.get_status('newest_genre_date')
+    last_genre_date = last_genre_date.decode(
+        'utf-8') if last_genre_date else '2000-01-01'
+    logger.debug('last_genre_date : %s', last_genre_date)
+    new_genre_date = f'{datetime.utcnow()}'
+    status.set_status('newest_genre_date', new_genre_date)
+    logger.debug('newest_genre_date after update : %s', new_genre_date)
+
+    for genre in pg_loader.get_new_genres(last_genre_date):
+        es_loader.save_genres(genre)
+
+    # в персонах попробовал другой loader
+    last_person_date = status.get_status('last_person_date')
+    last_person_date = last_person_date.decode(
+        'utf-8') if last_person_date else '2000-01-01'
+    logger.debug('last_person_date :: %s', last_person_date)
+    new_person_date = f'{datetime.utcnow()}'
+
+    persons = pg_loader.get_new_persons(last_person_date)
+    es_loader.save_persons(persons)
+    logger.debug('last_person_date :: %s', last_person_date)
+    status.set_status('last_person_date', new_person_date)
+
     status.disconnect()
 
 
 if __name__ == '__main__':
 
     while True:
+        logger.debug('ищем обновленные записи')
         transfer_data()
         sleep(10)
+        
