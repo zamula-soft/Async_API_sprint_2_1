6a2a7e6ed345f458ed8b811a1324edae779d79d1
кирилл бондарь <ironbondar@gmail.com>
2022-07-26 20:28:42 +0300

    api get all genres

--- yandex_project/fastapi-solution/api/v1/genres.py
+++ yandex_project/fastapi-solution/api/v1/genres.py
@@ -12,6 +12,25 @@ class Genre(BaseModel):
     name: str = None
 
 
+@router.get('/')
+async def get_all_genres(
+        count: int = 10,
+        order: str = 'desc',
+        genre_service: GenreService = Depends(get_genre_service),
+) -> Genre:
+
+    if count > 100:
+        count = 100
+
+    films = await genre_service.get_genres(count=count, order=order)
+    if not films:
+        # Если фильм не найден, отдаём 404 статус
+        # Желательно пользоваться уже определёнными HTTP-статусами, которые содержат enum
+        # Такой код будет более поддерживаемым
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='genre not found')
+    return films
+
+
 @router.get('/{genre_id}', response_model=Genre)
 async def genre_details(genre_id: str, genre_service: GenreService = Depends(get_genre_service)) -> Genre:
     genre = await genre_service.get_by_id(genre_id)
@@ -20,3 +39,14 @@ async def genre_details(genre_id: str, genre_service: GenreService = Depends(get
                             detail='genre not found')
     return Genre(
         **genre.dict())
+
+
+@router.get('/{genre_id}/films/', response_model=Genre)
+async def genre_details(genre_id: str, genre_service: GenreService = Depends(get_genre_service)) -> Genre:
+    films = await genre_service.get_movies_by_genre(genre_id)
+    if not films:
+        # Если фильм не найден, отдаём 404 статус
+        # Желательно пользоваться уже определёнными HTTP-статусами, которые содержат enum
+        # Такой код будет более поддерживаемым
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='genre not found')
+    return films
--- yandex_project/fastapi-solution/services/genres.py
+++ yandex_project/fastapi-solution/services/genres.py
@@ -23,6 +23,114 @@ class GenreService:
         self.redis = redis
         self.elastic = elastic
 
+    async def get_genres(self, count: int, order: str = 'desc'):
+
+        elastic_query = {
+            "size": count,
+            "query": {
+                "match_all": {}
+            },
+            "_source": 'false',
+            "fields": [
+                {
+                    "field": "name"
+                },
+                {
+                    "field": "id"
+                },
+
+            ],
+            "sort": [
+                {
+                    "rating": {
+                        "order": order,
+                        "missing": "_last",
+                        "unmapped_type": "float"
+                    }
+                }
+            ]
+        }
+        resp = await self.elastic.search(index='genres', body=elastic_query)
+        print('Get_top_genre_top_movies resp---', resp)
+
+        top_movies = resp['hits']['hits']
+        res = []
+        for m in top_movies:
+            res.append(
+                {
+                    'id': m['fields']['id'][0],
+                    'name': m['fields']['name'][0],
+                })
+
+        return res
+
+    async def get_movies_by_genre(self, genre_name: str = '', count: int = 10, order: str = 'desc'):
+        print('вошли в get_top_genre_top_movies ---')
+        print('genre_name ---- ', genre_name)
+
+        query = {
+            "match": {
+                "genres": {
+                    'id': genre_name,
+                },
+                }
+            }
+
+        if not genre_name:
+            query = {
+                    "match_all": {}
+                }
+
+        elastic_query = {
+                "size" : count,
+                "query" : query,
+                "_source" : 'false',
+                "fields" : [
+                    {
+                    "field" : "title"
+                    },
+                    {
+                    "field" : "genre"
+                    },
+                    {
+                    "field" : "rating"
+                    },
+                    {
+                        "field": "id"
+                    },
+                    {
+                        "field": ''
+                    }
+
+                ],
+                "sort" : [
+                    {
+                    "rating" : {
+                        "order" : order,
+                        "missing" : "_last",
+                        "unmapped_type" : "float"
+                    }
+                    }
+                ]
+                }
+        resp = await self.elastic.search(index='movies',body=elastic_query)
+        print('Get_top_genre_top_movies resp---', resp)
+        # for e in resp:
+        #     print('e---',e)
+        top_movies = resp['hits']['hits']
+        res = []
+        for m in top_movies:
+            # print('m---',m)
+            res.append(
+                {
+                    'id': m['fields']['id'][0],
+                    'title': m['fields']['title'][0],
+                    'rating': m['fields']['rating'][0],
+
+                })
+
+        return res
+
     async def get_by_id(self, genre_id: str) -> Optional[Genre]:
         """
         Get genre by id.
